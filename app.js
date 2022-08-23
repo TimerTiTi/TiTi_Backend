6fa79f99453cdda634c373d535b1daf5ad85b338
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-08-23 14:04:17 +0900

    [Feat] Controller: auth 추가

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ import express from "express";
 import { config } from "./config.js";
 import { sequelize } from "./db/database.js";
 import authRouter from "./router/auth.js";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan("tiny"));
 
 // Router
 app.use("/auth", authRouter);
