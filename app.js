46a02931fef3c68d8d82d344e8449f074c303fc9
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-08-23 13:15:35 +0900

    [Feat] Router: auth 추가

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from "express";
 import { config } from "./config.js";
 import { sequelize } from "./db/database.js";
+import authRouter from "./router/auth.js";
 
 const app = express();
 
+// Router
+app.use("/auth", authRouter);
+
+// 404
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+// ERROR
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+// Server start
 sequelize.sync().then(() => {
   console.log(`Server is started... ${new Date()}`);
   const server = app.listen(config.port);
