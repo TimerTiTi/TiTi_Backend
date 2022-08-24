cdf17e30944e83cde8ef15180973b50277a58d25
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-08-24 18:40:57 +0900

    [Feat] Daily class 추가

--- controller/daily.js
+++ controller/daily.js
@@ -1,21 +1,11 @@
 import {} from "express-async-errors";
+import { Daily } from "../class/daily.js";
 
 export async function createDailys(req, res) {
   const dailys = req.body;
-  dailys.forEach((daily) => {
-    console.log(daily);
-    const taskHistorys = daily.taskHistorys;
-
-    if (taskHistorys) {
-      Object.keys(taskHistorys).forEach((key) => {
-        const taskName = key;
-        taskHistorys[key].forEach((taskHistory) => {
-          console.log(
-            `${taskName}: ${taskHistory["startDate"]} ~ ${taskHistory["endDate"]}`
-          );
-        });
-      });
-    }
+  dailys.forEach((dailyObject) => {
+    const daily = new Daily(dailyObject);
+    daily.print();
   });
   res.sendStatus(201);
 }
