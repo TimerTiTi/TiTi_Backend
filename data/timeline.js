7a3784f6676ff4cee0533fdbd1a1ce3b56dcff4a
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-08-25 22:47:51 +0900

    [Feat] Timeline 정보 저장 구현 및 dailys, timelines Router 경로 수정

---
+++ data/timeline.js
@@ -0,0 +1,37 @@
+import { Timeline } from "../schema/timeline.js";
+
+export async function createTimeline(timeline, userId, dailyId) {
+  return Timeline.create({
+    time0: timeline[0],
+    time1: timeline[1],
+    time2: timeline[2],
+    time3: timeline[3],
+    time4: timeline[4],
+    time5: timeline[5],
+    time6: timeline[6],
+    time7: timeline[7],
+    time8: timeline[8],
+    time9: timeline[9],
+    time10: timeline[10],
+    time11: timeline[11],
+    time12: timeline[12],
+    time13: timeline[13],
+    time14: timeline[14],
+    time15: timeline[15],
+    time16: timeline[16],
+    time17: timeline[17],
+    time18: timeline[18],
+    time19: timeline[19],
+    time20: timeline[20],
+    time21: timeline[21],
+    time22: timeline[22],
+    time23: timeline[23],
+    userId,
+    dailyId,
+  }).then((data) => data.dataValues.id);
+}
+
+// 관리자용 api
+export async function getAll() {
+  return Timeline.findAll();
+}
--- router/timeline.js
+++ router/timeline.js
@@ -1,11 +1,10 @@
 import express from "express";
 import {} from "express-async-errors";
 import { isAuth } from "../middleware/auth.js";
-import * as dailyController from "../controller/daily.js";
+import * as timelineController from "../controller/timeline.js";
 
 const router = express.Router();
 
-router.post("/", isAuth, dailyController.createDailys);
-router.get("/", isAuth, dailyController.getAll);
+router.get("/", isAuth, timelineController.getAll);
 
 export default router;
