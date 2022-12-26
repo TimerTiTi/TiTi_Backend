5a2ce3c19ea6d2bf92b1e142a29401f56d3f63a8
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-12-26 13:21:56 +0900

    [Refactor] Daily Merge 로직 구현, 동작

--- controller/daily.js
+++ controller/daily.js
@@ -20,14 +20,14 @@ export async function createDailyTimelineTask(daily, req) {
   daily.status = "uploaded";
   // Daily 생성
   const createdDailyId = await dailyRepository.createDaily(daily, req.userId);
-  console.log(`CREATE Daily(${createdDailyId})`);
+  // console.log(`CREATE Daily(${createdDailyId})`);
   // Daily -> Tineline 생성
   const createdTimelineId = await timelineRepository.createTimeline(
     daily.timeline,
     req.userId,
     createdDailyId
   );
-  console.log(`CREATE Timeline(${createdTimelineId})`);
+  // console.log(`CREATE Timeline(${createdTimelineId})`);
   // Daily -> Task 생성
   for (const key in daily.tasks) {
     const createdTaskId = await taskRepository.createTask(
@@ -36,38 +36,50 @@ export async function createDailyTimelineTask(daily, req) {
       req.userId,
       createdDailyId
     );
-    console.log(`CREATE Task(${createdTaskId})`);
+    // console.log(`CREATE Task(${createdTaskId})`);
   }
 }
 
-export async function updateDailyTimelineAndDeleteCreateTask(DBdaily, req) {
+export async function updateDailyTimelineAndDeleteCreateTask(
+  id,
+  localDaily,
+  req
+) {
   // UPDATE Daily
-  const updatedDaily = await dailyRepository.updateDailyById(
-    DBDaily.id,
-    localDaily
-  );
-  console.log(`UPDATE Daily(${DBDaily.id})`);
+  const updatedDaily = await dailyRepository.updateDailyById(id, localDaily);
+  // console.log(`UPDATE Daily(${id})`);
   // UPDATE Timeline
   const updatedTimeline = await timelineRepository.updateTimelineByDailyId(
-    DBDaily.id,
+    id,
     localDaily
   );
-  console.log(`UPDATE Timeline(${updatedTimeline})`);
+  // console.log(`UPDATE Timeline(${updatedTimeline})`);
   // DELETE Tasks
   const deletedTasks = await taskRepository.deleteAllByDailyIdAndUserId(
-    DBDaily.id,
-    DBDaily.userId
+    id,
+    req.userId
   );
-  console.log(`DELETE  ${deletedTasks} Tasks from Daily(${DBDaily.id})`);
+  // console.log(`DELETE  ${deletedTasks} Tasks from Daily(${id})`);
   // CREATE Tasks
   for (const key in localDaily.tasks) {
     const createdTaskId = await taskRepository.createTask(
       key,
       localDaily.tasks[key],
       req.userId,
-      DBDaily.id
+      id
     );
-    console.log(`CREATE Task(${createdTaskId})`);
+    // console.log(`CREATE Task(${createdTaskId})`);
+  }
+}
+
+export async function findLastDaily(DBDaily, localDaily) {
+  // 마지막에 생성된 Daily 여야 하지만 로직이 매우 복잡해지므로 totalTime 값을 기준으로 설정
+  const dbTotalTime = DBDaily.timeline.reduce((a, b) => a + b, 0);
+  const localTotalTime = localDaily.timeline.reduce((a, b) => a + b, 0);
+  if (dbTotalTime > localTotalTime) {
+    return DBDaily;
+  } else {
+    return localDaily;
   }
 }
 
@@ -82,14 +94,28 @@ export async function uploadDailys(req, res) {
     // daily.id 값 확인
     if (localDaily.id == null) {
       // 같은 Day 의 Daily 가 있는지 확인
-      const daily = await dailyRepository.findBySameDate(
+      const dailys = await dailyRepository.findBySameDate(
         localDaily.day,
         32400,
         req.userId
       );
-      // 같은 Daily 가 존재하는 경우 -> UPDATE Daily
       // 같은 Daily 가 존재하지 않는 경우 -> CREATE Daily
-      await createDailyTimelineTask(localDaily, req);
+      if (dailys.length == 0) {
+        await createDailyTimelineTask(localDaily, req);
+        uploadCount += 1;
+        continue;
+      }
+      if (dailys.length > 1) {
+        console.log(`** Error[Same date dailys]: count = ${dailys.length}`);
+      }
+      // 같은 Daily 가 존재하는 경우 -> UPDATE Daily
+      const targetDaily = await findLastDaily(dailys[0], localDaily);
+      // console.log(`* MERGE DAILY (${targetDaily.id})`);
+      await updateDailyTimelineAndDeleteCreateTask(
+        targetDaily.id,
+        targetDaily,
+        req
+      );
       uploadCount += 1;
       continue;
     }
@@ -111,7 +137,7 @@ export async function uploadDailys(req, res) {
       continue;
     }
     // update Daily
-    await updateDailyTimelineAndDeleteCreateTask(DBDaily, req);
+    await updateDailyTimelineAndDeleteCreateTask(DBDaily.id, localDaily, req);
     uploadCount += 1;
   }
   // CREATE SyncLog
